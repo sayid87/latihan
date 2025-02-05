591871e196498921b1b57dd0afaae5255faab533
sayid87 <94373461+sayid87@users.noreply.github.com>
2025-02-05 14:43:12 +0700

    nambahin cors
    nambahin cors agar bisa diakses di front end

--- latihan/src/main/java/com/latihan/latihan/controllers/UserController.java
+++ latihan/src/main/java/com/latihan/latihan/controllers/UserController.java
@@ -11,15 +11,16 @@ import com.latihan.latihan.responses.WebResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private UserRepository uRep;
